b0a66ebab6a58d1083b42c2cd5716bd70d066790
vkolachi <36785379+vkolachi@users.noreply.github.com>
2023-12-04 16:06:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let a=prompt();
-let b=prompt();
+// // let a=prompt();
+// // let b=prompt();
 
-let z=a*2+3;
-alert("The final result is:"+z);
+
+// let z=parseint(a*2)+b;
+
+// alert("The final result is:"+z);
+
+
+var firstInput = prompt("Enter the first number:");
+var result = parseFloat(firstInput) * 2;
+
+// Take input for the second variable
+var secondInput = prompt("Enter the second number:");
+
+// Add the second input to the result of the first input multiplied by 2
+result = result + parseFloat(secondInput);
+
+// Display the final result using alert()
+alert("The final result is: " + result);
